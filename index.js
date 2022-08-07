4321f96551a49475f822517c5c5c81ec177e83be
kevin-fl <kevinflamang@gmail.com>
2022-08-07 15:07:44 +0200

    update

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const dataAPI = fetch("https://jsonplaceholder.typicode.com/photos");
 
+
 dataAPI.then(async(responseData) => {       //-> promise
 //console.log(responseData);
 
 
 const response = await responseData.json();
-console.log(response[1]);
+console.log(response[0]);
+// ctrl+maj+f -> donner le mot , le number  et changer ttes ces occurences , gain de temps 
 
 try{
 // objet 0 capture data API avec les 2 photos 
-const albumId = response[1].albumId;
-const id = response[1].id;
-const thumbnailUrl = response[1].thumbnailUrl;
-const title = response[1].title;
-const url = response[1].url;
-const Image = response[1].Image;
+const albumId = response[0].albumId;
+const id = response[0].id;
+const thumbnailUrl = response[0].thumbnailUrl;
+const title = response[0].title;
+const url = response[0].url;
+const Image = response[0].Image;
 
 //console.log(albumId);
 //console.log(id);
@@ -30,7 +32,7 @@ const affichage_id  = document.querySelector("#id");
 const affichage_thumbnailUrl  = document.querySelector("#thumbnailUrl");
 const affichage_title  = document.querySelector("#title");
 const affichage_url  = document.querySelector("#url");
-const affichage_image  = document.querySelector("#image01");
+const affichage_image  = document.querySelector("#image00");
 
 affichage_albumId.innerHTML = albumId;
 affichage_id.innerHTML = id;
@@ -41,13 +43,14 @@ affichage_url.innerHTML = url;
 
 //affichage des 2 photos
 
-const image01_thumbnailUrl = `<img src="${thumbnailUrl}">`;
-
+const image00_thumbnailUrl = `<a href="${url}" target="_blank"><img src="${thumbnailUrl}"></a>`;
+//target ="_blank" permet d ouvrir la photo dans un autre onglet 
 
 //affichage image 
 
-affichage_image.insertAdjacentHTML("afterbegin", image01_thumbnailUrl);
+affichage_image.insertAdjacentHTML("afterbegin", image00_thumbnailUrl);
 
+// NB : issue with the img viewing , probably caused by the website not by the method , err 522 , photos do not contain the keyword image ...
 
 } catch (err) {
 console.log(err);
